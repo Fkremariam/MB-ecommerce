8f89fd0664fb6a2f4f97bb84281752c0751a1121
Fkremariam <61139322+Fkremariam@users.noreply.github.com>
2022-12-21 17:34:48 +0300

    head added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import Head from '@next/head'
 import { client } from '../lib/client';
 import { Product, FooterBanner, HeroBanner } from '../components';
 
 const Home = ({ products, bannerData }) => (
   <div>
+      <title>MB-Fashion</title>
+      <metadata name='keyword' content="MB-Fahsion"/>
     <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
     <div className="products-heading">
       <h2>Best Seller Products</h2>
