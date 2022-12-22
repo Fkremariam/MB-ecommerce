03b464194214ba92a2b1e8d14e6036cd03bd1ff5
Fkremariam <61139322+Fkremariam@users.noreply.github.com>
2022-12-22 11:56:33 +0300

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/"><Image src='./logo.jpg' alt="MB-Fashion" width={127} height={65}/></Link>
+        <Link href="/">MB-Fashion</Link>
       </p>
 
       <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
